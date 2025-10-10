c651bed6cf45b8cd2b400004db45ddc5a7bbd269
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-10-10 02:15:33 -0600

    feat: conectar base de datos MYSQL

---
+++ server/database.js
@@ -0,0 +1,22 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = mysql
+    .createPool({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+    })
+    .promise();
+
+export async function getUserByID(id) {
+    const [row] = await pool.query(
+        `SELECT * FROM usuario WHERE id = ?`,
+        [id]
+    )
+   return row[0];
+}
+
+getUsuarioByID(1);
