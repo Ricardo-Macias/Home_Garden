884670027f62eaedc0c12bf785d00af22705ff7f
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-10-25 02:13:32 -0600

    feat: mostrar sensores en pantalla

--- home_garden/app/(tabs)/home.tsx
+++ home_garden/app/(tabs)/home.tsx
@@ -1,9 +1,31 @@
 import { Text, View, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import Constants from "expo-constants";
+
+interface AppConfig {
+    API_URL: string;
+}
+const config = Constants.expoConfig?.extra as AppConfig;
 
 export default function Home() {
+
+    const [users, setUser] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+    
+    async function fetchData() {
+        const response = await fetch(`${config.API_URL}/sensor/1`);
+        const data = await response.json();
+
+        setUser(data);
+    }
+
     return (
         <View style={styles.container}>
             <Text> Bienvenido </Text>
+            <Text>{ JSON.stringify(users) }</Text>
         </View>
     );
 }
