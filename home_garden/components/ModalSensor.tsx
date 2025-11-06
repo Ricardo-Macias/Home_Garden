fec7691df3efacaf4a4f9365a50e30cb57ec10e1
Ricardo <ricardo.macias1755@alumnos.udg.mx>
2025-11-06 13:33:56 -0600

    feat: redireccionar al formulario agregar sensor

--- home_garden/components/ModalSensor.tsx
+++ home_garden/components/ModalSensor.tsx
@@ -1,4 +1,5 @@
 import { Modal, View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 type Props = {
@@ -7,7 +8,27 @@ type Props = {
     onClose: () => void;
 };
 
-export default function ModalSensor({ isVisible, children, onClose}: Props){
+export default function ModalSensor({ isVisible, children, onClose }: Props){
+    const router = useRouter();
+    const sensors = [{
+            id: 1,
+            name: "sensor 1"
+        },{
+            id: 2,
+            name: "sensor 2"
+        },{
+            id: 3,
+            name: "sensor 3"
+        },{
+            id: 4,
+            name: "sensor 4"
+        }
+    ];
+
+    const handleSensor = () => {
+        router.push("/sensor/AddSensor");
+    };
+
     return (
         <Modal animationType="slide" transparent={true} visible={isVisible}>
             <View style={styles.modalContent}>
@@ -20,6 +41,15 @@ export default function ModalSensor({ isVisible, children, onClose}: Props){
                     </TouchableOpacity>
                 </View>
                 {children}
+                <View style={styles.sensorsContainer}>
+                    {
+                        sensors.map((item) => <TouchableOpacity
+                            style={styles.sensorsItem}
+                            onPress={handleSensor}>
+                                <Text style={styles.sensorItemText}>{item.name}</Text>
+                            </TouchableOpacity>)
+                    }
+                </View>
             </View>
         </Modal>
     )
@@ -48,7 +78,25 @@ const styles = StyleSheet.create({
     title: {
         color: "#fff",
         fontSize: 16,
+    },
+    sensorsContainer: {
+        flex: 1,
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+    },
+    sensorsItem: {
+        backgroundColor: "blue",
+        padding: 5,
+        margin: 5,
+        height: "20%",
+        width: "45%",
+    },
+    sensorItemText: {
+        fontSize: 25,
+        color: "#fff",
+        textAlign: "center",
+        textAlignVertical: "center",
     }
-
 
 })
