f8c36a72c27136de05aa6bd6871e594d0aafb28e
jacueline <jacqueline.macias64411@alumnos.udg.mx>
2025-11-10 16:57:41 -0600

    feat: agregar direcciones a componentes y perfil

--- home_garden/app/(tabs)/perfile.tsx
+++ home_garden/app/(tabs)/perfile.tsx
@@ -1,9 +1,40 @@
-import { Text, View, StyleSheet } from 'react-native';
+import React from "react";
+import { useRouter } from "expo-router";
+import { ScrollView, StyleSheet } from "react-native";
+import ComponentsProfile from "../../components/Perfile/ComponentsProfile";
+import ComponentsHistory from "../../components/Perfile/ComponentsHistory";
+import ComponentsSettings from "../../components/Perfile/ComponentsSettings";
+import { getUserProfile } from "../Profile/UserProfile";
+import { getGardenHistory } from "../Profile/GardenHistory";
 
-export default function Perfile() {
+export default function PerfilScreen() { 
+    const userData = getUserProfile();
+    const gardenData = getGardenHistory();
+
+    const router = useRouter();
+
+    const handleLogout = () => {};
+    const handleViewHistory = () => {};
+    const handleSettings = () => {
+        router.push("/Profile/Settings");
+    };
+
     return (
-        <View >
-            <Text>Perfil</Text>
-        </View>
+        <ScrollView contentContainerStyle={styles.container}>
+            <ComponentsProfile
+                user={userData}
+                onLogout={handleLogout}
+                onViewHistory={handleViewHistory}
+                onSettings={handleSettings}
+            />
+            <ComponentsHistory data={gardenData} />
+        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 16,
+        backgroundColor: "#fff",
+    },
+});
