c530ea8d9794ca23d9105d8e8ac3cea72c3244de
jacueline <jacqueline.macias64411@alumnos.udg.mx>
2025-12-01 19:06:01 -0600

    feat: anadir funciones para conectar la carpeta store

--- home_garden/app/_layout.tsx
+++ home_garden/app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store"; 
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </PersistGate>
+    </Provider>
   );
 }
