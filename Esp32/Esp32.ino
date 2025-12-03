f339be1157c2f9aa9c929729ee62abdf103995a0
Ricardo <ricardo.macias1755@alumnos.udg.mx>
2025-12-03 16:32:00 -0600

    feat: agregar funciones WIFI al Esp32

--- Esp32/Esp32.ino
+++ Esp32/Esp32.ino
@@ -1,16 +1,94 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <WiFi.h>
+
+#define WIFI_SERVICE_UUID   "79f0dfb3-5f08-4502-8ff0-5a9b2396aab1"
+#define WIFI_SSID_CHAR_UUID "d24233d0-b34d-4bbe-8f2a-f661530c6217"
+#define WIFI_PASS_CHAR_UUID "ef1c20a3-26b6-4aab-88dc-e377f105e8f4"
+
+String receivedSSID = "";
+String receivedPassword = "";
+
+/*
+  Conectar WiFI
+*/
+void connectToWiFi() {
+  Serial.println("\n Intentando conectar a WiFI...");
+  WiFi.begin(receivedSSID.c_str(), receivedPassword.c_str());
+
+  int timeout = 0;
+
+  while (WiFi.status() != WL_CONNECTED && timeout < 20){
+    delay(500);
+    Serial.print(".");
+    timeout++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\n conectado a WiFi");
+    Serial.println("IP: " + WiFi.localIP().toString());
+  } else {
+    Serial.println("\n Error al conectar a WiFI");
+  }
+}
+
+/*
+  CALLBACK 
+*/
+
+class WifiCharCallback : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *characteristic){
+    String value = characteristic->getValue().c_str();
+
+    if (characteristic->getUUID().toString() == WIFI_SSID_CHAR_UUID){
+      receivedSSID = String(value.c_str());
+      Serial.println("SSID recivido: " + receivedSSID);
+    }
+
+    else if (characteristic->getUUID().toString() == WIFI_PASS_CHAR_UUID) {
+      receivedPassword= String(value.c_str());
+      Serial.println("Password recibido: " + receivedPassword);
+
+      if (receivedSSID.length() > 0 && receivedPassword.length() > 0) {
+        connectToWiFi();
+      }
+    }
+  }
+};
+
+/*
+  Iniciar Bluetooth
+*/
 
 void setupBluetooth() {
   BLEDevice::init("HomeGarden");
 
+  BLEServer *server = BLEDevice::createServer();
+
+  BLEService *wifiService = server->createService(WIFI_SERVICE_UUID);
+
+  // SSID
+  BLECharacteristic *ssidChar = wifiService->createCharacteristic(
+    WIFI_SSID_CHAR_UUID,
+    BLECharacteristic::PROPERTY_WRITE
+  );
+  ssidChar->setCallbacks(new WifiCharCallback());
+
+  //Contraseña
+  BLECharacteristic *passChar = wifiService->createCharacteristic(
+    WIFI_PASS_CHAR_UUID,
+    BLECharacteristic::PROPERTY_WRITE
+  );
+  passChar->setCallbacks(new WifiCharCallback());
+
+  wifiService->start();
+
   BLEDevice::startAdvertising();
-  Serial.println("Bluetooth iniciado");
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   setupBluetooth();
 }
 
