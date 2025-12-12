a0878022c2479d3f8c3c18b5b68d7d57935bb1a1
jacueline <jacqueline.macias64411@alumnos.udg.mx>
2025-12-11 18:37:27 -0600

    chore: implementar redux store en _layout

--- home_garden/app/_layout.tsx
+++ home_garden/app/_layout.tsx
@@ -1,17 +1,14 @@
 import { Stack } from "expo-router";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "../store/store"; 
+import { store } from "../Redux/store";
 
 export default function RootLayout() {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </PersistGate>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
     </Provider>
   );
 }
