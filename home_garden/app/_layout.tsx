4a8ab45ed04e9a3cc0d61e4d4232c8924c13c385
Ricardo <ricardo.macias1755@alumnos.udg.mx>
2025-12-15 19:16:20 -0600

    chore: agregar el SafeAreaView

--- home_garden/app/_layout.tsx
+++ home_garden/app/_layout.tsx
@@ -1,11 +1,14 @@
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{
-        headerShown: false,
-      }} />
-    </Stack>
+    <SafeAreaView style={{flex: 1,}}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{
+          headerShown: false,
+        }} />
+      </Stack>
+    </SafeAreaView>
   );
 }
