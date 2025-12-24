e540ec5f07b4703626dc070c85d5d1724d25168b
Ricardo <ricardo.macias1755@alumnos.udg.mx>
2025-12-23 19:18:36 -0600

    feat: Guardar nombre, SSID y password en la memoria flash

--- Esp32/Esp32.ino
+++ Esp32/Esp32.ino
@@ -1,20 +1,64 @@
+#include <Preferences.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <WiFi.h>
 
-#define WIFI_SERVICE_UUID   "79f0dfb3-5f08-4502-8ff0-5a9b2396aab1"
-#define WIFI_SSID_CHAR_UUID "d24233d0-b34d-4bbe-8f2a-f661530c6217"
-#define WIFI_PASS_CHAR_UUID "ef1c20a3-26b6-4aab-88dc-e377f105e8f4"
+Preferences prefs;
 
-String receivedSSID = "";
-String receivedPassword = "";
+#define WIFI_SERVICE_UUID "e72640a5-7d6f-401a-b506-8355a871f404"
+#define WIFI_SSID_CHAR_UUID "92f0538e-66f2-48f4-bf43-94e3d3fdf475"
+#define WIFI_PASS_CHAR_UUID "53c48d51-2386-4584-8a8b-eb17ad324324"
+
+String deviceName;
+
+String receivedSSID;
+String receivedPassword;
+
+/*
+  Generar nombre del esp32
+*/
+
+String getDeviceName(){
+  uint64_t chipid = ESP.getEfuseMac();
+  char name[20];
+  sprintf(name, "HomeGarden-%04X",(uint16_t)(chipid & 0xFFFF));
+  return String(name);
+}
+
+/*
+  Guardar el Nombre del ESP32
+*/
+
+void saveConfig(){
+  prefs.begin("device", false);
+
+  deviceName = prefs.getString("name", "");
+  receivedSSID = prefs.getString("ssid", "");
+  receivedPassword = prefs.getString("password", "");
+
+  if (deviceName == ""){
+    //Primera vez encendido
+    deviceName = getDeviceName();
+    prefs.putString("name", deviceName);
+  }else{
+    Serial.println("Nombre: " + deviceName);
+  }
+
+  if (receivedSSID == ""){
+    setupBluetooth();
+  } else {
+    connectToWiFi();
+  }
+
+  prefs.end();
+}
 
 /*
   Conectar WiFI
 */
 void connectToWiFi() {
-  Serial.println("\n Intentando conectar a WiFI...");
+  //Serial.println("\n Intentando conectar a WiFI...");
   WiFi.begin(receivedSSID.c_str(), receivedPassword.c_str());
 
   int timeout = 0;
@@ -28,9 +72,17 @@ void connectToWiFi() {
   if (WiFi.status() == WL_CONNECTED) {
     Serial.println("\n conectado a WiFi");
     Serial.println("IP: " + WiFi.localIP().toString());
+    prefs.begin("device", false);
+
+    prefs.putString("ssid", receivedSSID);
+    prefs.putString("password", receivedPassword);
+
+    prefs.end();
   } else {
     Serial.println("\n Error al conectar a WiFI");
   }
+
+
 }
 
 /*
@@ -62,7 +114,7 @@ class WifiCharCallback : public BLECharacteristicCallbacks {
 */
 
 void setupBluetooth() {
-  BLEDevice::init("HomeGarden");
+  BLEDevice::init(deviceName);
 
   BLEServer *server = BLEDevice::createServer();
 
@@ -89,7 +141,8 @@ void setupBluetooth() {
 
 void setup() {
   Serial.begin(115200);
-  setupBluetooth();
+  delay(1000);
+  saveConfig();
 }
 
 void loop() {
