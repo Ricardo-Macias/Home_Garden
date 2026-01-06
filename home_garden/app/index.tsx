b2bea7d7b179d66a36b71526f984eefd2213f3df
Ricardo <ricardo.macias1755@alumnos.udg.mx>
2026-01-05 18:02:15 -0600

    chore: redireccionar a home

--- home_garden/app/index.tsx
+++ home_garden/app/index.tsx
@@ -1,39 +1,9 @@
-import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import Constants from "expo-constants";
+import { Buffer } from "buffer";
+global.Buffer = Buffer;
 
-interface AppConfig {
-  API_URL: string;
-}
-
-const config = Constants.expoConfig?.extra as AppConfig;
+import { Redirect } from "expo-router";
+import { useState } from "react";
 
 export default function Index() {
-  const [users, setUser] = useState([]);
-
-  useEffect(() => {
-      fetchData();
-    },[])
-
-  async function fetchData() {
-    const response = await fetch(`${config.API_URL}/user/1`);
-    const data = await response.json();
-
-    setUser(data);
-
-  
-  }
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Bienvenido a Home Garden, Ricardo</Text>
-      <Text>{ JSON.stringify(users) }</Text>
-    </View>
-  );
+  return <Redirect href="/home" />;
 }
