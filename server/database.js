a7a62f528e4057b6189d8fe072347ccb2edacc72
Ricardo <ricardo.macias1755@alumnos.udg.mx>
2026-01-14 15:55:35 -0600

    fix: cambiar la ip a deviceName en las consultas a la base de datos

--- server/database.js
+++ server/database.js
@@ -116,17 +116,17 @@ export async function getSensors(idUsuario) {
 
 export async function searchForSensorByName(name) {
     const [row] = await pool.query(
-        `SELECT * FROM sensor WHERE nombre = ?`,
+        `SELECT * FROM sensor WHERE deviceName = ?`,
         [name]
     );
-    const sensorID = result.insertId;
+    const sensorID = row[0].idSensor;
     return sensorID;
 }
 
-export async function insertSensor(idUsuario, ip) {
+export async function insertSensor(idUsuario, name) {
     const [result] = await pool.query(
-        `INSERT INTO sensor (idUsuario, ip) VALUES (?, ?)`,
-        [idUsuario, ip]
+        `INSERT INTO sensor (idUsuario, deviceName) VALUES (?, ?)`,
+        [idUsuario, name]
     );
     const sensorID = result.insertId;
     return sensorID;
