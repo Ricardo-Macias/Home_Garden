cd1e233e17aa494ed1fc77a50c5376acb7522ec5
jacueline <jacqueline.macias64411@alumnos.udg.mx>
2026-02-08 13:54:49 -0600

    feat: agregar colores al fonde de la aplicacion

---
+++ home_garden/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: "#7FC563",       
+    accent: "#C8A2C8",        
+    background: "#F5F5F5",     // Fondo
+    cardBackground: "#FFFFFF", // Tarjetas
+    text: "#333333", 
+    textSecondary: "#666666",
+};
