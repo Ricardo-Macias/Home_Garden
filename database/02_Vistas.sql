d6a57860e8b922f33c7bd2f3e2216d3c2c19cbce
Criterius-eng <criteriusdeadspace@gmail.com>
2026-03-25 17:56:20 -0600

    feat: crear vista para el historial de riego

--- database/02_Vistas.sql
+++ database/02_Vistas.sql
@@ -29,3 +29,14 @@ CREATE VIEW vista_historial_huertos AS SELECT
     JOIN usuario u ON s.idUsuario = u.id
     JOIN cultivo c ON h.idCultivo = c.id
     WHERE h.estado = 1;
+
+CREATE VIEW vw_historialRegado AS SELECT
+    u.id AS idUsuario,
+    h.idHuerto AS idHuerto,
+    r.idRiego AS idRiego,
+    s.idSensor AS idSensor,
+    s.nombre AS sensor
+    FROM huerto h
+    JOIN sensor s ON h.idSensor = s.idSensor
+    JOIN usuario u ON s.idUsuario = u.id
+    JOIN historial_regado r ON r.idHuerto = h.idHuerto;
