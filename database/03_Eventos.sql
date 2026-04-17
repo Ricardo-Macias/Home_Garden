b3805bdbf005de39740a1b07506177ccfdc839d8
Ricardo <ricardo.macias1755@alumnos.udg.mx>
2026-04-16 19:45:23 -0600

    chore: comentar evento

--- database/03_Eventos.sql
+++ database/03_Eventos.sql
@@ -1,13 +1,10 @@
-/*SET GLOBAL event_scheduler = ON;
-
-USE home_garden;
-
-CREATE EVENT IF NOT EXISTS marcar_huertos_finalizados
-ON SCHEDULE EVERY 1 DAY
-DO
-    UPDATE huerto
-    SET estado = 1, "fechaFin" = CURRENT_DATE
-    WHERE "fechaEstimada" IS NOT NULL
-    AND "fechaEstimada" <= CURRENT_DATE
-    AND estado = 0;
+/*SELECT cron.schedule(
+    'marcar_huertos_finalizados',
+    '0 0 * * *',
+    $$UPDATE huerto
+        SET estado = 1, "fechaFin" = CURRENT_DATE
+        WHERE "fechaEstimada" IS NOT NULL
+        AND "fechaEstimada" <= CURRENT_DATE
+        AND estado = 0;$$
+);
 */
